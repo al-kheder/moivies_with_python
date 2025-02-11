45b81773477f379b0bd5e96a7a5c7dfb54cd387d
ali al-kheder <alkhdyyr@gmail.com>
2025-02-11 05:45:05 +0100

    create movie search, sort,stats,update

--- movies.py
+++ movies.py
@@ -1,4 +1,4 @@
-
+import random
 # list of the movies
 movies = [
     {"name": "In the Name of the Father", "year": 1993, "rating": 8.1},
@@ -27,12 +27,56 @@ def add_movie():
     movie = {"name": name, "year": year, "rating": rating}
     movies.append(movie)
 
+#delete a movie from the list by name of the movie
+def delete_movie():
+    name = str(input("Enter the name of the movie you want to delete: ")).lower().strip()
+    for movie in movies:
+        if movie["name"] == name:
+            movies.remove(movie)
+            print(f"{name} has been removed from the list")
+        else :
+            print(f"{name} is not in the list")
 
+# update a movie in the list
+def update_movie():
+    name = str(input("Enter the name of the movie you want to update: ")).lower().strip()
+    for movie in movies:
+        if movie["name"] == name:
+            rating = float(input("Enter the new rating of the movie: "))
+            movie["rating"] = rating
+            print(f"{name} has been updated")
+        else:
+            print(f"{name} is not in the list")
 
+#stats of the movies
+def stats():
+    total_movies = len(movies)
+    total_rating = sum([movie["rating"] for movie in movies])
+    avg_rating = total_rating / total_movies
+    print(f"Total number of movies: {total_movies}")
+    print(f"Total rating of the movies: {total_rating}")
+    print(f"Average rating of the movies: {avg_rating}")
+    print("Median rating of the movies: ", sorted([movie["rating"] for movie in movies])[len(movies) // 2])
 
+#random movie
+def random_movie():
+    movie = random.choice(movies)
+    print(f"Name: {movie['name']}, Year: {movie['year']}, Rating: {movie['rating']}")
 
+#Search for a part of the movie name
+def search_movie():
+    search = str(input("Enter the name of the movie you want to search: ")).lower().strip()
+    for movie in movies:
+        if search in movie["name"]:
+            print(f"Name: {movie['name']}, Rating: {movie['rating']}")
+        else:
+            print(f"{search} is not in the list")
 
-
+#movies sorted by rating
+def movies_sorted_by_rating():
+    sorted_movies = sorted(movies, key=lambda movie: movie["rating"], reverse=True)
+    for movie in sorted_movies:
+        print(f"Name: {movie['name']}, Rating: {movie['rating']}")
 
 def menu():
     print(
@@ -56,6 +100,7 @@ def menu():
         """
     )
     add_movie()
+    delete_movie()
     list_movies(movies)
 
 
